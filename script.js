6aa88198a597580476bdfec1e7b274d426fa0bc1
Kyle Perales <kyleperalestudio@gmail.com>
2023-10-19 11:43:28 +0800

    Move squares into grid container

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const mainContainer = document.querySelector('#main-container')
+const gridContainer = document.querySelector('#grid-container')
+
 createGrid(16)
 
 function createGrid(count) {
   gridCount = count * count
   for(i=0; i<gridCount; i++) {
     const square = document.createElement('div')
-    mainContainer.appendChild(square)
+    gridContainer.appendChild(square)
   }
 }
