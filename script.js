75236b25776660a67f2ee82be1312f7774f155f2
Kyle Perales <kyleperalestudio@gmail.com>
2023-10-19 11:46:41 +0800

    Add eventListener for hover

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const mainContainer = document.querySelector('#main-container')
 const gridContainer = document.querySelector('#grid-container')
+const hoverColor = '#000000'
 
 createGrid(16)
 
@@ -7,6 +8,9 @@ function createGrid(count) {
   gridCount = count * count
   for(i=0; i<gridCount; i++) {
     const square = document.createElement('div')
+    square.addEventListener('mouseover', (e) => {
+      square.setAttribute('style', `background-color: ${hoverColor}`)
+    }, false)
     gridContainer.appendChild(square)
   }
 }
